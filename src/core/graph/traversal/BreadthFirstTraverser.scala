c018447d36e10658ff5d2f79947aa7cdef938086
RamsesDeNorre <ramses.rommel@gmail.com>
2011-11-02 19:22:35 +0100

    Rename (bis).

--- src/core/graph/traversal/BreadthFirstTraverser.scala
+++ src/core/graph/traversal/BreadthFirstTraverser.scala
@@ -8,9 +8,8 @@ import core.graph.{DirectedGraph, Edge, Vertex}
  * Date: 29/10/11
  * Time: 17:47
  */
-@deprecated("Useless?", "0")
-class BreadthFirstTraverser[V <: Vertex, E <: Edge[V]] (graph: DirectedGraph[V, E])
-  extends GraphTraverser[V, E](graph) {
+class BreadthFirstTraverser[V <: Vertex, E <: Edge[V]]
+  (graph: DirectedGraph[V, E]) extends GraphTraverser[V, E](graph) {
 
   def foreach[U](f: (V) => U) {
     traverse(f)(graph.someVertex)
@@ -18,10 +17,10 @@ class BreadthFirstTraverser[V <: Vertex, E <: Edge[V]] (graph: DirectedGraph[V, 
 
   private def traverse[U](f: (V) => U)(v: V) {
     f(v)
-    graph.neighbours(v) foreach traverse(f)
+    (graph neighbours v) foreach traverse(f)
   }
 
   def test[U](f: (V) => U)(v: V) {
-    var list = (graph.neighbours(v) filter (v!=)).toList
+    var list = ((graph neighbours v) filter (v!=)).toList
   }
 }

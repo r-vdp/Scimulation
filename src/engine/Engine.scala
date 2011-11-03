8d1173ae06390d53e1d31691b0d4975d47b6b9af
rdebruyne <mail@debruyne.me>
2011-11-03 14:27:26 +0100

    engine v2

--- src/engine/Engine.scala
+++ src/engine/Engine.scala
@@ -1,15 +1,36 @@
 package engine
 
 import core.graph.{Graph, Edge, Vertex}
+import java.util.PriorityQueue
 
-
-trait Transactions {
-  def list: List[Transaction];
+trait Time{
+  val time:Integer
+  
+  def getTime : Integer = time
+  
 }
 
-class Transaction {
-  def areAble[V <: Vertex](stakeholders: List[V with Action[V]]): Boolean =
+trait Event extends Transaction with Time{
+  
+}
+
+trait Transactions[V <: Vertex] {
+  def list: List[Transaction];
+  
+  def doTransactions() = {
+    for (t <-list) {
+      if(t.areAble){
+        t.doTransaction
+      }
+    }
+  }
+}
+
+class Transaction(f: Seq[Vertex] => Seq[Vertex],stakeholders: List[Vertex with Action[Vertex]]) {
+  def areAble[V <: Vertex]: Boolean =
     (true /: stakeholders) (_ && _.isAble)
+  
+  def doTransaction = {};
 }
 
 trait Action[V <: Vertex] {
@@ -18,28 +39,49 @@ trait Action[V <: Vertex] {
   def execute(): V with Action[V];
 }
 
+
 class Engine[V <: Vertex, E <: Edge[V]](graph: Graph[V, E]) {
 
   type AV = V with Action[V]
-
+  type TV = V with Transactions[V]
+  
+  
+  var time:Integer = 0
+  var eventList:PriorityQueue[Event];
+  
+  
   def run(f: Seq[V] => Seq[V], count: Int) {
     var vertices = graph.toSeq
-    for (_ <- 0 until count) {
+    for (time <- 0 until count) {
       vertices = f(vertices)
     }
   }
 
-  //
+  // list nodes => node heeft queue transac, gaan die af,
   
-  def roundBased(list: List[V]): List[V] = {
-    list
+  def roundBased(list: List[TV]): List[TV] = {
+     for(t<-list){
+       t.doTransactions
+     }
+     list
   }
 
-  def TurnBased(list: List[AV]): List[AV] = {
-    list.head.execute() :: TurnBased(list.tail)
+  def turnBased(list: List[AV]): List[AV] = {
+    list.head.execute() :: turnBased(list.tail)
   }
 
-  def EventBased(list: List[V]): List[V] = {
+  def eventBased(list: List[V]): List[V] = {
+   
+    
+    while(time == eventList.peek().getTime){
+      eventList.poll.doTransaction
+    }
+    
     list
   }
+  
+  def addEvent(event:Event)={
+	  eventList.add(event)
+  }
+  
 }

241b02035b0f4abc97ee74eb4cf134d6c0b7e9b9
rdebruyne <mail@debruyne.me>
2011-11-18 20:33:03 +0100

    examples

--- src/example/social/EventBasedSocialSimulation.scala
+++ src/example/social/EventBasedSocialSimulation.scala
@@ -18,16 +18,37 @@ object EventBasedSocialSimulation extends App {
 
     GraphRepository.loadGraph(graph,"social.xml")
 
-    val engine = new EventBasedEngine(graph, 5)
+    val engine = new EventBasedEngine(graph, 12)
 
     engine.addEvent(new Friendship(1,graph,graph.getVertex("2").get,graph.getVertex("0").get))
+    engine.addEvent(new Friendship(1,graph,graph.getVertex("4").get,graph.getVertex("0").get))
+    engine.addEvent(new Friendship(2,graph,graph.getVertex("0").get,graph.getVertex("5").get))
+    engine.addEvent(new Friendship(2,graph,graph.getVertex("6").get,graph.getVertex("1").get))
     engine.addEvent(new Friendship(3,graph,graph.getVertex("4").get,graph.getVertex("0").get))
-    engine.addEvent(new Friendship(4,graph,graph.getVertex("0").get,graph.getVertex("5").get))
-
+    engine.addEvent(new Friendship(3,graph,graph.getVertex("9").get,graph.getVertex("13").get))
+    engine.addEvent(new Friendship(3,graph,graph.getVertex("12").get,graph.getVertex("11").get))
+    engine.addEvent(new Friendship(3,graph,graph.getVertex("4").get,graph.getVertex("11").get))
+    engine.addEvent(new Friendship(4,graph,graph.getVertex("7").get,graph.getVertex("9").get))
+    engine.addEvent(new Friendship(4,graph,graph.getVertex("9").get,graph.getVertex("0").get))
+    engine.addEvent(new Friendship(4,graph,graph.getVertex("10").get,graph.getVertex("0").get))
+    engine.addEvent(new Friendship(5,graph,graph.getVertex("3").get,graph.getVertex("5").get))
+    engine.addEvent(new Friendship(5,graph,graph.getVertex("7").get,graph.getVertex("5").get))
+    engine.addEvent(new Friendship(6,graph,graph.getVertex("4").get,graph.getVertex("12").get))
+    engine.addEvent(new Friendship(6,graph,graph.getVertex("11").get,graph.getVertex("5").get))    
+    engine.addEvent(new Friendship(7,graph,graph.getVertex("14").get,graph.getVertex("7").get))
+    engine.addEvent(new Friendship(7,graph,graph.getVertex("2").get,graph.getVertex("8").get))
+    engine.addEvent(new Friendship(8,graph,graph.getVertex("8").get,graph.getVertex("9").get))
+    engine.addEvent(new Friendship(8,graph,graph.getVertex("1").get,graph.getVertex("10").get))
+    engine.addEvent(new Friendship(9,graph,graph.getVertex("1").get,graph.getVertex("11").get))
+    engine.addEvent(new Friendship(9,graph,graph.getVertex("1").get,graph.getVertex("0").get))    
+    engine.addEvent(new Friendship(10,graph,graph.getVertex("4").get,graph.getVertex("3").get))
+    engine.addEvent(new Friendship(11,graph,graph.getVertex("3").get,graph.getVertex("2").get))
+    engine.addEvent(new Friendship(11,graph,graph.getVertex("9").get,graph.getVertex("6").get))    
+    
     val vis = new UbiGraphVisualizer[SocialActor, SocialEdge[SocialActor]]
     vis.subscribeTo(engine)
     
-    val snapshots = new SnapshotCreator[SocialActor, SocialEdge[SocialActor]](10,"EvSocout")
+    val snapshots = new SnapshotCreator[SocialActor, SocialEdge[SocialActor]](3,"EvSocout")
     snapshots.subscribeTo(engine)
 
 

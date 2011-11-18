864666f78d25d5b1b8ac5049177c8a4d98209432
5t0rm <storm@5T0RM-R700.(none)>
2011-11-18 16:14:24 +0100

    Introduced Dave.

--- src/example/virus/TurnBasedVirusSimulation.scala
+++ src/example/virus/TurnBasedVirusSimulation.scala
@@ -16,48 +16,83 @@ object TurnBasedVirusSimulation extends App {
     rootMap += "status" -> Status.I
     rootMap += "probability" -> 3
     rootMap += "gender" -> Gender.Male
-    val root = VirusActor("root", rootMap)
+    rootMap += "progSkill" -> 0
+    val root = VirusActor("1", rootMap)
 
     var secondMap: Map[String, Any] = Map.empty
     secondMap += "status" -> Status.S
     secondMap += "probability" -> 6
     secondMap += "gender" -> Gender.Female
-    val second = VirusActor("second", secondMap)
+    secondMap += "progSkill" -> 0
+    val second = VirusActor("2", secondMap)
 
     var thirdMap: Map[String, Any] = Map.empty
     thirdMap += "status" -> Status.NI
     thirdMap += "probability" -> 2
     thirdMap += "gender" -> Gender.Male
-    val third = VirusActor("third", thirdMap)
+    thirdMap += "progSkill" -> 0
+    val third = VirusActor("3", thirdMap)
 
     var fourthMap: Map[String, Any] = Map.empty
     fourthMap += "status" -> Status.S
     fourthMap += "probability" -> 7
     fourthMap += "gender" -> Gender.Female
-    val fourth = VirusActor("fourth", fourthMap)
+    fourthMap += "progSkill" -> 0
+    val fourth = VirusActor("4", fourthMap)
 
     var fifthMap: Map[String, Any] = Map.empty
     fifthMap += "status" -> Status.S
     fifthMap += "probability" -> 5
-    rootMap += "gender" -> Gender.Male
-    val fifth = VirusActor("fifth", fifthMap)
+    fifthMap += "gender" -> Gender.Male
+    fifthMap += "progSkill" -> 0
+    val fifth = VirusActor("5", fifthMap)
 
-    val vertices = root :: second :: third :: fourth :: fifth :: Nil
+    var sixMap: Map[String, Any] = Map.empty
+    sixMap += "status" -> Status.S
+    sixMap += "probability" -> 5
+    sixMap += "gender" -> Gender.Male
+    sixMap += "progSkill" -> 0
+    val six = VirusActor("6", sixMap)
+
+    var sevenMap: Map[String, Any] = Map.empty
+    sevenMap += "status" -> Status.S
+    sevenMap += "probability" -> 5
+    sevenMap += "gender" -> Gender.Male
+    sevenMap += "progSkill" -> 0
+    val seven = VirusActor("7", sevenMap)
+
+    var eightMap: Map[String, Any] = Map.empty
+    eightMap += "status" -> Status.S
+    eightMap += "probability" -> 5
+    eightMap += "gender" -> Gender.Male
+    eightMap += "progSkill" -> 0
+    val eight = VirusActor("8", eightMap)
+
+    var daveMap: Map[String, Any] = Map.empty
+    daveMap += "status" -> Status.S
+    daveMap += "probability" -> 5
+    daveMap += "gender" -> Gender.Male
+    daveMap += "name" -> "Dave"
+    val dave = VirusActor("Dave", daveMap)
+
+    val vertices = root :: second :: third :: fourth :: fifth :: six :: seven :: eight :: dave :: Nil
 
     graph.addVertices(vertices)
 
     val edges = VirusEdge(root, second) ::
                 VirusEdge(second, third) ::
-                VirusEdge(root, third) ::
+                VirusEdge(root, dave) ::
                 VirusEdge(second, fourth) ::
                 VirusEdge(root, fourth) ::
                 VirusEdge(third, fourth) ::
-                VirusEdge(root, fifth) :: Nil
+                VirusEdge(dave, fifth) ::
+                VirusEdge(fifth, six) ::
+                VirusEdge(dave, seven) ::
+                VirusEdge(dave, eight) :: Nil
 
     graph.addEdges(edges)
 
-    val engine = new TurnBasedEngine(graph, 5)
-
+    val engine = new TurnBasedEngine(graph, 20)
     val vis = new UbiGraphVisualizer[VirusActor, VirusEdge[VirusActor]]
 
     vis.subscribeTo(engine)
--- src/example/virus/VirusActor.scala
+++ src/example/virus/VirusActor.scala
@@ -29,23 +29,26 @@ class VirusActor(inId: String, inMap: Map[String, Any])
 
   override def color: String = {
     if (getStatus == Status.I) {
-    	"#00ff00";
-    } else if (getGender == Gender.Female) {
-      "#0000ff";
-    }else{
       "#ff0000"
-    }
+    } else if (getName == "Dave") {
+      "#ff00ff"
+    } else {
+      println((255 - getProgSkill % 255))
+      "#" + (255 - getProgSkill % 255).toHexString + "ff" + (255 - getProgSkill % 255).toHexString
+    } 
   }
 
   object AllDone extends Exception { }
 
   override def execute() {
-    if (getStatus == Status.S) {
+    if (getStatus == Status.S && getName != "Dave") {
       try{
 	      neighbours.foreach{e=>
 	        if(e.getStatus==Status.I){
-	          infect();
+	          infect
 	          throw AllDone
+	        } else if(e.getName=="Dave"){
+	          incProgSkill
 	        }
 	      }
       } catch {
@@ -62,11 +65,32 @@ class VirusActor(inId: String, inMap: Map[String, Any])
   def getGender = params.get("gender") getOrElse "unknown"
 
   def getProbability = params.get("probability") getOrElse "unknown"
+  
+  def getName = params.get("name") getOrElse "unknown"
+  
+  def getProgSkill: Int = {
+    val skill = params.get("progSkill") getOrElse "0"
+    Integer.parseInt(skill.toString())
+    
+/*    try {
+      Integer.parseInt(params.get("progSkill").toString())
+    } catch {
+      case _ => 0
+    }
+*/  }
 
   def setStatus(newStatus: Status) {
     params += ("status" -> newStatus)
   }
-
+  
+  def setName(name: String) {
+    params += ("name" -> name)
+  }
+  
+  def setProgSkill(level: Int) {
+	params += ("progSkill" -> level)
+  }
+  
   def die() {
     setStatus(Status.R)
     println("Actor: " + id + "  with status " + getStatus + " and gender " +
@@ -83,6 +107,10 @@ class VirusActor(inId: String, inMap: Map[String, Any])
     setStatus(Status.I)
     println("Actor: " + id + "  with status " + getStatus + " and gender " +
             getGender + " just got infected");
+  }
+  
+  def incProgSkill() {
+    setProgSkill(getProgSkill + 10)
   }
 }
 
